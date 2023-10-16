6b2fd99037038c089161a2fb6992af715c39dd2d
Debdutta Chatterjee <debduttachatterjee09@gmail.com>
2023-10-16 11:52:24 +0530

    Add project files.

---
+++ Test_SendEmail/UnitTest1.cs
@@ -0,0 +1,42 @@
+namespace Test_SendEmail
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.Fail();
+        }
+
+
+        [Test]
+        public void Test5()
+        {
+            Assert.Fail();
+        }
+    }
+}
